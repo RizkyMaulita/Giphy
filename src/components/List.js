d758d3c0ae821c1e30ae00061c6042b9d7476a5d
RizkyMaulita <rizky.maulita@gmail.com>
2021-08-22 00:22:37 +0700

    add alt

--- src/components/List.js
+++ src/components/List.js
@@ -9,7 +9,7 @@ const List = ({ keyword, error, gifs, loading, currentPage, totalPages, getGifs 
   return (
     <div>
       {loading
-       ?  <img src={loadingGif} />
+       ?  <img src={loadingGif} alt="gif" />
        :  <div>
             <div className="d-flex justify-content-center px-2 row row-cols-1 row-cols-md-4 row-cols-sm-3">
               {gifs?.map((gif, index) =><img key={index} src={gif} alt="gif" style={{ width: '400px', height: '200px', margin: '20px' }} />)}
